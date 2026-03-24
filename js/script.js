599323072ae185139ebc456bb758a286950cfc54
Beata Wierzbicka <b.wierzbicka90@gmail.com>
2026-03-24 11:15:45 -0400

    related posts list - now JS list

--- js/script.js
+++ js/script.js
@@ -23,13 +23,11 @@
     return postCard;
   }
   
-
 /********************************************************
   
                         INDEX PAGE
                         
 ********************************************************/ 
-
 
 /**********  FEATURED POSTS  **********/
 
@@ -48,7 +46,6 @@
     });
   });
 
-
 /**********  SEASONAL CARE **********/
 
   const seasonalContainer = document.getElementById("seasonal-posts");
@@ -65,8 +62,7 @@
     });
   });
 
-
-/********************************************************
+/*******************************************************
   
                         BLOG PAGE
 
@@ -82,6 +78,46 @@
       });
     });
 
-
-
+/*******************************************************
   
+                        BLOG POST - RELATED ARTICLES
+
+********************************************************/ 
+
+  const relatedArticles = document.getElementById("related-posts");
+
+  fetch("../data/posts.json")
+    .then(response => response.json())
+    .then(posts => {
+    
+      // Find current post
+      const currentPath = window.location.pathname;
+      const currentPost = posts.find(post => {
+        return currentPath.includes(post.link.replace("./", ""));
+      })
+
+      // Find related posts 
+      const relatedPosts = posts.filter(post => {
+        // exclude current post
+        if (post.link === currentPost.link) 
+          return false;
+        // check tag match
+        //.some() is an array method that checks if at least one element passes a test
+        //array.some(element => condition) - returns true/false
+          return post.tags.some(tag => currentPost.tags.includes(tag));
+      });
+
+      relatedPosts.slice(0, 4).forEach(post => {
+          const relatedPost = document.createElement("a");
+          relatedPost.classList.add("rp-link");
+          relatedPost.href = "."+ post.link;
+          relatedPost.innerHTML = `
+                <img src=".${post.image}" alt="${post.alt}">
+                <span>${post.title}</span>
+            `;
+
+      relatedArticles.appendChild(relatedPost);
+      });
+
+    })
+          
